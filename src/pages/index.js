ac4047f86ac0bb96c18f5f2e404bf29db8f773e1
Benjamin Glitsos <bengyup@gmail.com>
2020-05-30 23:39:26 +1000

    improved project folder structure and imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import { useEffect, Fragment } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import Page from "../components/Page";
-import { addCount } from "../store/count/action";
-import AddCount from "../components/AddCount";
-import { wrapper } from "../store/store";
+import Page from "~/components/Page";
+import { addCount } from "~/store/count/action";
+import AddCount from "~/components/AddCount";
+import { wrapper } from "~/store/store";
 
 const Index = props => {
     return (
