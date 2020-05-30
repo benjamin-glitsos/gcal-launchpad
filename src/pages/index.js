94a11627434bbe39aa629ed6ea3a407e5a94e2ed
Benjamin Glitsos <bengyup@gmail.com>
2020-05-30 13:45:01 +1000

    setting up next-redux-wrapper

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,3 @@
-import withRedux from "next-redux-wrapper";
-
-const Home = ({ foo, custom }) => (
-    <div>
-        <div>Prop from Redux {foo}</div>
-        <div>Prop from getInitialProps {custom}</div>
-    </div>
-);
-
-Home.getInitialProps = ({ store, isServer, pathname, query }) => {
-    // component will read from store's state when rendered
-    store.dispatch({ type: "FOO", payload: "foo" });
-    // pass some custom props to component from here
-    return { custom: "custom" };
-};
-
-Home = withRedux(makeStore, state => ({ foo: state.foo }))(Home);
-
-export default Home;
+export default function Home() {
+    return <p>lorem</p>;
+}
