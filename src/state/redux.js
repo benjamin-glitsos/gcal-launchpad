4aec1872032722116fc082ea5bc015f06c51563f
Benjamin Glitsos <bengyup@gmail.com>
2020-05-31 19:17:22 +1000

    refactored redux file somewhat

--- src/state/redux.js
+++ src/state/redux.js
@@ -1,27 +1,25 @@
 import { combineReducers } from "redux";
 import { createUpdater } from "redux-lightweight";
 
-export class Input {
-    state = "";
-
-    updateInput(data) {
-        return data;
-    }
-}
-
 export const [updateInputReducer, updateInputActions] = createUpdater(
-    Input
+    class Input {
+        state = "";
+
+        updateInput(data) {
+            return data;
+        }
+    }
 );
 
-export class History {
-    state = [];
+export const [getHistoryReducer, getHistoryActions] = createUpdater(
+    class History {
+        state = [];
 
-    getHistory(data) {
-        return data;
+        getHistory(data) {
+            return data;
+        }
     }
-}
-
-export const [getHistoryReducer, getHistoryActions] = createUpdater(History);
+);
 
 export const rootReducer = combineReducers({
     input: updateInputReducer,
