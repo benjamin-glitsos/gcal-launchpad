48c90ce90e955d7c19205b76a8da6219592a2b31
Benjamin Glitsos <bengyup@gmail.com>
2020-05-31 23:15:02 +1000

    removed unnecesary user method

--- src/state/redux.js
+++ src/state/redux.js
@@ -6,10 +6,6 @@ export const [userReducer, userActions] = createUpdater(
         state = {
             username: "default"
         };
-
-        updateUser({ username }) {
-            return { username };
-        }
     }
 );
 
