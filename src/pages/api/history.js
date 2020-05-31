edfc195acfef254e5b8a2bcf804314e8b75bb97f
Benjamin Glitsos <bengyup@gmail.com>
2020-05-31 17:09:37 +1000

    updated api to join in the new users table

--- src/pages/api/history.js
+++ src/pages/api/history.js
@@ -2,7 +2,13 @@ import { pool } from "~/lib/database";
 
 export default async (req, res) => {
     try {
-        const query = await pool.query(`SELECT * FROM history`);
+        const query = await pool.query(`
+            SELECT h.title
+            FROM history h
+            LEFT JOIN users u ON h.user_id = u.id
+            ORDER BY h.id DESC
+            LIMIT 15
+            `);
         res.status(200).json(query.rows);
     } catch (e) {
         console.error(e);
