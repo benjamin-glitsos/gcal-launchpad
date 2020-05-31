1c39c1ac786275494e1dcf18ce264968eed5e5bf
Benjamin Glitsos <bengyup@gmail.com>
2020-05-31 13:49:43 +1000

    removed clock

--- components/page.js
+++ components/page.js
@@ -1,32 +1,30 @@
-import Link from 'next/link'
-import { useSelector } from 'react-redux'
+import Link from "next/link";
+import { useSelector } from "react-redux";
 
-import Counter from './counter'
-import Clock from './clock'
+import Counter from "./counter";
 
 function Page({ linkTo, NavigateTo, title }) {
-  const placeholderData = useSelector((state) => state.placeholderData)
-  const error = useSelector((state) => state.error)
-  const light = useSelector((state) => state.light)
-  const lastUpdate = useSelector((state) => state.lastUpdate)
-  return (
-    <div>
-      <h1>{title}</h1>
-      <Clock lastUpdate={lastUpdate} light={light} />
-      <Counter />
-      <nav>
-        <Link href={linkTo}>
-          <a>Navigate: {NavigateTo}</a>
-        </Link>
-      </nav>
-      {placeholderData && (
-        <pre>
-          <code>{JSON.stringify(placeholderData, null, 2)}</code>
-        </pre>
-      )}
-      {error && <p style={{ color: 'red' }}>Error: {error.message}</p>}
-    </div>
-  )
+    const placeholderData = useSelector(state => state.placeholderData);
+    const error = useSelector(state => state.error);
+    const light = useSelector(state => state.light);
+    const lastUpdate = useSelector(state => state.lastUpdate);
+    return (
+        <div>
+            <h1>{title}</h1>
+            <Counter />
+            <nav>
+                <Link href={linkTo}>
+                    <a>Navigate: {NavigateTo}</a>
+                </Link>
+            </nav>
+            {placeholderData && (
+                <pre>
+                    <code>{JSON.stringify(placeholderData, null, 2)}</code>
+                </pre>
+            )}
+            {error && <p style={{ color: "red" }}>Error: {error.message}</p>}
+        </div>
+    );
 }
 
-export default Page
+export default Page;
