1684e128b3bbe4cd14dd464f814433ec3b9f0356
Benjamin Glitsos <bengyup@gmail.com>
2020-06-02 22:19:41 +1000

    finished enough of the parser for now

--- src/lib/parser.js
+++ src/lib/parser.js
@@ -34,25 +34,16 @@ const words = sepBy1(whitespace)(notWhitespaces);
 
 const anything = regex(/^.*/);
 
-const now = moment(new Date(), process.env.settings.timeZone);
-
 const createDay = (optionalNumber = 0, unit) => {
     const isUnit = s => unit === s;
+    const now = moment(new Date(), process.env.settings.timeZone);
+    const future = momentUnit => now.add(optionalNumber, momentUnit);
     return cond([
         { case: x => !optionalNumber, return: now },
-        {
-            case: isUnit("d"),
-            return: now.add(optionalNumber, "days").format("yyyy-mm-dd")
-        },
-        {
-            case: isUnit("w"),
-            return: now.add(optionalNumber, "weeks")
-        },
-        {
-            case: isUnit("m"),
-            return: now.add(optionalNumber, "months")
-        },
-        { case: isUnit("y"), return: now.add(optionalNumber, "years") }
+        { case: isUnit("d"), return: future("days") },
+        { case: isUnit("w"), return: future("weeks") },
+        { case: isUnit("m"), return: future("months") },
+        { case: isUnit("y"), return: future("years") }
     ])(true);
 };
 
@@ -81,160 +72,9 @@ const event = coroutine(function* () {
 });
 
 export default function parser(s) {
-    // TODO: make this run the redux success or failure actions
     return event.fork(
         s,
-        (error, parsingState) => {
-            // TODO: remove this console.error ?
-            console.error(error);
-            return s;
-        },
+        (error, parsingState) => s,
         (result, parsingState) => result
     );
 }
-
-// const parser = R.pipe(sanitise, s =>
-//     event.fork(
-//         s,
-//         (error, parsingState) => error,
-//         (result, parsingState) => result
-//     )
-// );
-
-// const symbols = new Pairs({
-//     t: "today",
-//     h: "hours",
-//     d: "days",
-//     w: "weeks",
-//     m: "months",
-//     q: "quarters",
-//     y: "years"
-// });
-//
-// const timeZone = "Australia/Sydney";
-//
-// const now = () => moment(new Date(), timeZone);
-//
-// const today = () => now().startOf("day");
-//
-// const unwords = R.join(" ");
-//
-// const unchars = R.join("");
-//
-// const whitespaces = many(whitespace);
-//
-// const notWhitespaces = many1(anythingExcept(whitespace)).map(unchars);
-//
-// const words = sepBy1(whitespace)(notWhitespaces);
-//
-// console.log(words.run("")); // TODO: get this to fail for no input
-//
-// const sanitise = R.trim;
-//
-// const number = coroutine(function* () {
-//     const p = {};
-//
-//     p.integer = [yield possibly(char("-")), yield digits];
-//
-//     p.fractional = yield possibly(sequenceOf([char("."), digits]));
-//
-//     return R.pipe(Object.values, R.flatten, parseInt)(p);
-// });
-//
-// const optionalNumber = possibly(number);
-//
-// const unitSymbol = choice(symbols.keys.map(c => char(c)));
-//
-// const optionalUnitSymbol = possibly(unitSymbol);
-//
-// const ifNil = R.curry((b, a) => a || b);
-//
-// const error = s => {
-//     throw new Error(s);
-// };
-//
-// const surround = (b, a) => b + a + b;
-//
-// const exclamations = many(char("!"));
-//
-// const event = coroutine(function* () {
-//     const importanceHead = yield exclamations;
-//
-//     const lead = yield optionalNumber.map(ifNil(1));
-//
-//     const unit = yield optionalUnitSymbol.map(ifNil(symbols.key("today")));
-//
-//     const duration = yield optionalNumber.map(ifNil(0));
-//
-//     const secondUnit = yield optionalUnitSymbol;
-//
-//     const importanceTail = yield exclamations;
-//
-//     const space = yield whitespaces;
-//
-//     const importance = R.sum(
-//         [importanceHead, importanceTail].map(p => p.map(R.length))
-//     );
-//
-//     const importanceFormat = R.curry(s => {
-//         switch (importance) {
-//             case 0:
-//                 return s;
-//                 break;
-//             case 1:
-//                 return s.toUpperCase();
-//                 break;
-//             case 2:
-//             default:
-//                 // TODO: use some function to incrementally wrap functions based on whether it satisfies the cases. e.g. surround(toUpper)
-//                 return surround("!!!", s.toUpperCase());
-//         }
-//     });
-//
-//     const summary = yield words.map(unwords).map(importanceFormat);
-//
-//     console.log(`(${summary.length})`);
-//
-//     // const calcDate = ({ lead, unit, duration, secondUnit }) =>
-//     //     R.cond([
-//     //         [
-//     //             R.equals(symbols.key("today")),
-//     //             R.always(makeDateTimes({ lead: null, unit, duration: null }))
-//     //         ],
-//     //         [R.includes(R.__, symbols.keys), makeDateTimes],
-//     //         [R.T, unit => error(`Invalid time/date unit in user input: ${unit}`)]
-//     //     ])(unit);
-//
-//     const isTime = [unit, secondUnit].includes("h");
-//
-//     const momentAdd = (m, n, unit) => m.add(n, symbols.key(unit));
-//
-//     const start = momentAdd(isTime ? now() : today(), lead, unit);
-//
-//     const end = momentAdd(start, duration, secondUnit || unit);
-//
-//     const dateOrTime = `date${isTime ? "Time" : ""}`;
-//
-//     const dayFormat = "YYYY-MM-DD";
-//
-//     const hoursFormat = null;
-//
-//     const momentFormat = m => m.format(isTime ? hoursFormat : dayFormat);
-//
-//     const createDate = date => ({
-//         [dateOrTime]: momentFormat(date),
-//         timeZone
-//     });
-//
-//     const date = R.map(createDate, { start, end });
-//
-//     return { summary, ...date };
-// });
-//
-// const parser = R.pipe(sanitise, s =>
-//     event.fork(
-//         s,
-//         (error, parsingState) => error,
-//         (result, parsingState) => result
-//     )
-// );
