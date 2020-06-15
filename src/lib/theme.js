fef7755198ba2c6b8256443946421bbb24272bdc
Benjamin Glitsos <bengyup@gmail.com>
2020-06-15 23:47:23 +1000

    now styling starting from material preset and using deepmerge to overwrite some of its values

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -1,68 +1,51 @@
-import rebass from "@rebass/preset";
 import material from "@rebass/preset-material";
+import merge from "deepmerge";
 
-export default {
-    ...rebass,
-    ...material,
-    ...{
-        breakpoints: ["0", "1000px"],
-        colors: {
-            text: "#3c4043",
-            background: "#fff",
-            primary: "#1a73e8",
-            modes: {
-                sending: {},
-                success: {},
-                failure: {}
-            }
+const custom = {
+    colors: {
+        text: "#3c4043",
+        background: "#fff",
+        primary: "#1a73e8",
+        modes: {
+            sending: {},
+            success: {},
+            failure: {}
+        }
+    },
+    text: {
+        h1: {
+            color: "primary",
+            fontWeight: "bold",
+            fontSize: 4
         },
-        fonts: (() => {
-            const defaults = "Google Sans, Roboto, Arial, sans-serif";
-            return { body: defaults, heading: defaults };
-        })(),
-        lineHeights: {
-            body: 1.5,
-            heading: 1.25
-        },
-        radii: {
-            default: 4
-        },
-        shadows: {
-            default: "0 0 4px rgba(0, 0, 0, .125)"
-        },
-        text: {
-            h1: {
-                color: "primary"
-            },
-            h2: {
-                fontSize: 3
-            }
-        },
-        buttons: {
-            primary: {
-                fontWeight: "bold",
-                color: "background",
-                bg: "primary",
-                borderRadius: "default",
-                px: 3
-            }
-        },
-        forms: {
-            mainInput: {
-                color: "primary",
-                fontSize: 3,
-                px: 3,
-                py: 2,
-                my: 4
-            }
-        },
-        styles: {
-            root: {
-                maxWidth: ["auto", 1000],
-                mx: "auto",
-                px: 3,
-                py: 4
-            }
+        h2: {
+            fontSize: 3
+        }
+    },
+    forms: {
+        mainInput: {
+            color: "primary",
+            fontWeight: "bold",
+            fontSize: 3,
+            px: 3,
+            py: 2,
+            my: 4
+        }
+    },
+    styles: {
+        root: {
+            maxWidth: ["auto", 1000],
+            mx: "auto",
+            px: 3,
+            py: 4
         }
     }
 };
+
+export default (() => {
+    const overwriteMerge = (destinationArray, sourceArray, options) =>
+        sourceArray;
+    return merge.all([material, custom], {
+        arrayMerge: overwriteMerge
+    });
+})();
