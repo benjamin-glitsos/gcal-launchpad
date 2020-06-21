227a601f45996b36857cb1e8f91b9dd2e8ce0504
Benjamin Glitsos <bengyup@gmail.com>
2020-06-21 13:07:59 +1000

    styling

--- src/components/divider.js
+++ src/components/divider.js
@@ -1,12 +1,13 @@
 import { Box } from "rebass";
 
 export default function Divider(props) {
+    const color = props.color;
     return (
         <Box
             {...props}
             as="hr"
             sx={{
-                bg: "muted",
+                bg: props.color || "muted",
                 border: 0,
                 height: 1,
                 mb: 2
