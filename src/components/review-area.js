d0dc381ffd8e21689ccfc6654d0a3aa326165065
Benjamin Glitsos <bengyup@gmail.com>
2020-06-23 00:18:52 +1000

    made review area buttons disappear when there are no applicable events

--- src/components/review-area.js
+++ src/components/review-area.js
@@ -8,35 +8,39 @@ import ReviewCard from "~/components/review-card";
 export default function ReviewArea() {
     const dispatch = useDispatch();
     const events = useSelector(review.selectors.events);
+    const activeEvents = events.filter(
+        event => event.status !== process.env.messages.DELETED
+    );
     const hasEvents = events.length > 0;
-    const ids = events.map(event => event.id);
+    const hasActiveEvents = activeEvents.length > 0;
+    const activeIds = activeEvents.map(event => event.id);
     return (
         hasEvents && (
             <Flex my={2}>
                 <Box width={1}>
-                    <Flex mb={3}>
+                    <Flex mb={3} height={40}>
                         <Box width={1}>
                             <ButtonBar
                                 list={[
                                     {
                                         title: "Delete All",
-                                        isDisplayed: true,
+                                        isDisplayed: hasActiveEvents,
                                         variant: "outline",
                                         onClick: () =>
                                             dispatch(
                                                 review.actions.toDeleteMultiple(
-                                                    ids
+                                                    activeIds
                                                 )
                                             )
                                     },
                                     {
                                         title: "Send All",
-                                        isDisplayed: true,
+                                        isDisplayed: hasActiveEvents,
                                         variant: "primary",
                                         onClick: () =>
                                             dispatch(
                                                 review.actions.sendMultiple(
-                                                    events
+                                                    activeEvents
                                                 )
                                             )
                                     }
