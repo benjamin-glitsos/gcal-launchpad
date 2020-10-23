215ddd8821dac182ed682aa9f26d7ad2d6d2d128
Benjamin Glitsos <bengyup@gmail.com>
2020-10-23 18:35:55 +1100

    Fixed side margins

--- src/components/page.js
+++ src/components/page.js
@@ -3,11 +3,17 @@ import { Box } from "rebass";
 export default function Page(props) {
     return (
         <Box
-            {...props}
             sx={{
                 maxWidth: ["auto", 1000],
                 mx: "auto"
             }}
-        />
+        >
+            <Box
+                {...props}
+                sx={{
+                    mx: 3
+                }}
+            />
+        </Box>
     );
 }
