4a0b590cb02351836fc21f4c5b7b43d57a8b3f8c
Benjamin Glitsos <bengyup@gmail.com>
2020-10-23 18:52:37 +1100

    Refactored index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import Head from "next/head";
+import { default as NextHead } from "next/head";
 import { Heading, Flex, Box } from "rebass";
 import ReviewArea from "~/components/review-area";
 import EventInput from "~/components/event-input";
@@ -9,43 +9,49 @@ import MainMenu from "~/components/main-menu";
 import PatternBackground from "~/components/pattern-background";
 import Page from "~/components/page";
 
+const Head = () => (
+    <NextHead>
+        <title>{process.env.settings.title}</title>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    </NextHead>
+);
+
+const Body = () => (
+    <Fragment>
+        <Page pt={4} pb={2}>
+            <Flex alignItems="center" flexWrap="wrap">
+                <Box width={[1, 1 / 2]} pb={[3, 1]}>
+                    <Heading variant="h1" textAlign={["center", "left"]}>
+                        {process.env.settings.title}
+                    </Heading>
+                </Box>
+                <Box width={[1, 1 / 2]}>
+                    <MainMenu />
+                </Box>
+            </Flex>
+        </Page>
+        <PatternBackground py={2}>
+            <Page>
+                <Flex my={3}>
+                    <Box width={1}>
+                        <EventInput />
+                    </Box>
+                </Flex>
+                <ReviewArea />
+                <InfoCard />
+            </Page>
+        </PatternBackground>
+        <Page>
+            <HistoryList title="Try these examples:" />
+        </Page>
+    </Fragment>
+);
+
 export default function Index() {
     return (
         <Fragment>
-            <Head>
-                <title>{process.env.settings.title}</title>
-                <link
-                    rel="shortcut icon"
-                    href="/favicon.ico"
-                    type="image/x-icon"
-                />
-            </Head>
-            <Page pt={4} pb={2}>
-                <Flex alignItems="center" flexWrap="wrap">
-                    <Box width={[1, 1 / 2]} pb={[3, 1]}>
-                        <Heading variant="h1" textAlign={["center", "left"]}>
-                            {process.env.settings.title}
-                        </Heading>
-                    </Box>
-                    <Box width={[1, 1 / 2]}>
-                        <MainMenu />
-                    </Box>
-                </Flex>
-            </Page>
-            <PatternBackground py={2}>
-                <Page>
-                    <Flex my={3}>
-                        <Box width={1}>
-                            <EventInput />
-                        </Box>
-                    </Flex>
-                    <ReviewArea />
-                    <InfoCard />
-                </Page>
-            </PatternBackground>
-            <Page>
-                <HistoryList title="Try these examples:" />
-            </Page>
+            <Head />
+            <Body />
         </Fragment>
     );
 }
