c0cd0d85548b35024e241453b96e362531d974f8
Benjamin Glitsos <bengyup@gmail.com>
2020-10-23 17:50:20 +1100

    Pattern is now complete

--- src/components/pattern-background.js
+++ src/components/pattern-background.js
@@ -2,9 +2,6 @@ import { Box } from "rebass";
 
 export default function PatternBackground(props) {
     return (
-        <Box
-            {...props}
-            sx={{ backgroundImage: "url(/star-pattern-tile.svg)" }}
-        />
+        <Box {...props} sx={{ backgroundImage: "url(/pattern-tile.svg)" }} />
     );
 }
